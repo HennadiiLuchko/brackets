c63babd5f439111f21866e7fb8c3a0858c68bf1d
HennadiiLuchko <137803124+HennadiiLuchko@users.noreply.github.com>
2023-08-08 21:53:47 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   let result = str;
+
+      for (let i = 0; i < str.length; i++){ 
+             result = result.replaceAll("[]","")
+                            .replaceAll("{}","")
+                            .replaceAll("()","")
+                            .replaceAll("||","")
+                            .replaceAll("12","")
+                            .replaceAll("34","")
+                            .replaceAll("56","")
+                            .replaceAll("77","")
+                            .replaceAll("88","");  
+      }
+    
+    return result.length === 0 ? true: false;
 }
